64243d1c9fe1004d0107b8cb80579b468e862009
lasthitaki <alexhitaki@gmail.com>
2023-04-14 20:08:54 -0300

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://192.168.1.105:3000/',
+  baseURL: 'http://192.168.100.113:3000',
 });
 
 export default instance;
---
+++ styles/theme.jsx
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+
+const theme = {
+  colors: {
+
+  },
+};
+export default theme;
