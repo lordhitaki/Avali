27b53ccacd10b7803f4b7c1e76a9fc61b53327b8
lasthitaki <alexhitaki@gmail.com>
2023-04-25 11:36:25 -0300

    Update

--- src/routes/paginas/addTarefas/index.tsx
+++ src/routes/paginas/addTarefas/index.tsx
@@ -32,7 +32,7 @@ type FormDataProps = {
 
 let dataValidation =
   /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
-let horaValidation = /^(?:([01]\d|2[0-3]).([0-5]\d))$/;
+let horaValidation = /^(?:([01]\d|2[0-3]):([0-5]\d))$/;
 
 export default function AddTask() {
   const navigation = useNavigation();
